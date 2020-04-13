1bb3518dd09d44571c05e1cc525cc760f873aeb8
New91 <niu91@qq.com>
2020-04-13 09:52:07 +0800

    1. 重载了拷贝构造函数和拷贝赋值函数，并在其中实现了管理权限转移，防止资源重复释放 2. 模拟普通指针* 和 ->操作

--- SmartPointer.hpp
+++ SmartPointer.hpp
@@ -10,6 +10,36 @@ public:
 	{
 		delete _ptr;
 	}
+
+	SmartPointer(SmartPointer<T>& other)
+	{
+		//管理权限转移，否则会造成重复释放的问题
+		_ptr = other._ptr;
+		other._ptr = NULL;
+	}
+	SmartPointer& operator=(SmartPointer<T>& other)
+	{
+		if (this != other)
+		{
+			//管理权限转移，否则会造成重复释放的问题
+			_ptr = other._ptr;
+			other._ptr = NULL;
+		}
+
+		return *this;
+	}
+
+	// 模拟普通指针的操作
+	T& operator*()
+	{
+		return *_ptr;
+	}
+
+	T* operator->()
+	{
+		return _ptr;
+	}
+
 protected:
 	T* _ptr;
 };
--- test_main.cpp
+++ test_main.cpp
@@ -5,12 +5,20 @@ using namespace std;
 class Demo {
 public:
 	Demo(int d) :data(d) {}
+	int func() {
+		cout << __FUNCTION__ << endl;
+		return 0;
+	}
 private:
 	int data;
 };
 
 int test_smartpointer() {
 	SmartPointer<Demo> pdemo(new Demo(100));
+	SmartPointer<Demo> pdemo2 = pdemo;
+	pdemo2->func();
+	(*pdemo2).func();
+	
 	return 0;
 }
 int main()
